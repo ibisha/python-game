3043dcd26acb6859d421fc188d1f9ab333726a4b
ibisha <akashiroakaaka@gmail.com>
2018-01-08 00:09:06 +0900

    円描画

---
+++ draw_circle.py
@@ -0,0 +1,29 @@
+""" draw circle """
+import sys
+import pygame
+from pygame.locals import QUIT, Rect
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))
+FPSCLOCK = pygame.time.Clock()
+
+def main():
+    """ main """
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        SURFACE.fill((255, 255, 255))
+
+        pygame.draw.circle(SURFACE, (255, 0, 0), (50, 50), 20)
+        pygame.draw.circle(SURFACE, (255, 0, 0), (150, 50), 20, 10)
+        pygame.draw.circle(SURFACE, (0, 255, 0), (50, 150), 10)
+        pygame.draw.circle(SURFACE, (0, 255, 0), (150, 150), 20)
+
+        pygame.display.update()
+        FPSCLOCK.tick(10)
+
+if __name__ == "__main__":
+    main()
