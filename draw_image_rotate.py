baef1d9ca7156c051e2894dbfe28f84c85251d34
ibisha <akashiroakaaka@gmail.com>
2018-01-08 12:56:34 +0900

    画像回転

---
+++ draw_image_rotate.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+from pygame.locals import QUIT
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))
+FPSCLOCK = pygame.time.Clock()
+
+def main():
+    logo = pygame.image.load("images/pythonlogo.jpg")
+    theta = 0
+
+    while True:
+        for event in pygame.event.get(QUIT):
+            pygame.quit()
+            sys.exit()
+
+        theta += 1
+        SURFACE.fill((225, 225, 225))
+
+        # rotate logo
+        rotated_logo = pygame.transform.rotate(logo, theta)
+        SURFACE.blit(rotated_logo, (100, 30))
+
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+if __name__ == '__main__':
+    main()
